3a249f88878e8a4872f4bdd1e6386c9d2beb6a72
Kacper1104 <kacper.kozimor@gmail.com>
2022-12-18 19:34:34 +0100

    typo fix

--- frontend/src/routes/NewOffer.tsx
+++ frontend/src/routes/NewOffer.tsx
@@ -121,7 +121,7 @@ const NewOffer = (newOfferState: NewOfferState) => {
           <button
             onClick={submit}
             className={'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl'}>
-            Save
+            Zapisz
           </button>
         </div>
       </div>
